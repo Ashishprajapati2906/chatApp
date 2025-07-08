405522dec2262273748b78a88ac5e0a77f0ca761
Ashishprajapati2906 <prajapatiashu2906@gmail.com>
2025-07-08 16:33:46 +0530

    chat page work start

---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProfileUpdate.css'
+
+const ProfileUpdate = () => {
+  return (
+      <div>We are on the ProfileUpdate</div>
+  )
+}
+
+export default ProfileUpdate
